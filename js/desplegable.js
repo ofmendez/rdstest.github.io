8665e1e5bee98212a2dce56f7d08b2c281dff9a0
Julianrds <practica.desarrollo@reddesignsystems.com>
2022-02-22 15:04:09 -0500

    actualizacion de textos, correccion de responsive y texto desplegable

---
+++ js/desplegable.js
@@ -0,0 +1,7 @@
+
+let btnDesplegable = document.getElementById("btnDesplegable");
+let divDesplegable = document.getElementById("divDesplegable");
+
+btnDesplegable.addEventListener("click", function(){
+divDesplegable.classList.toggle("mostrar");});    
+    
