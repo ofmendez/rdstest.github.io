cca465c346f97e9e7ec8f2545bd68599eb80dcd3
Julianrds <practica.desarrollo@reddesignsystems.com>
2022-02-24 17:59:48 -0500

    ajuste tamaño de letra, adicion de texto. correccion de responsive. delete desplegable

--- js/desplegable.js
+++
@@ -1,7 +0,0 @@
-
-let btnDesplegable = document.getElementById("btnDesplegable");
-let divDesplegable = document.getElementById("divDesplegable");
-
-btnDesplegable.addEventListener("click", function(){
-divDesplegable.classList.toggle("mostrar");});    
-    
